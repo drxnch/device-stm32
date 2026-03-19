54a7021e9d8beae7286dd4985c447392b7ba64f7
Deven <deven.ranchhod3@gmail.com>
2026-03-19 16:04:58 +1300

    Add Unity and CMock as plain folders

---
+++ Firmware/Tests/vendor/CMock/test/system/test_compilation/callingconv.h
@@ -0,0 +1,12 @@
+/* =========================================================================
+    CMock - Automatic Mock Generation for C
+    ThrowTheSwitch.org
+    Copyright (c) 2007-26 Mike Karlesky, Mark VanderVoord, & Greg Williams
+    SPDX-License-Identifier: MIT
+========================================================================= */
+
+#ifndef __stdcall
+#define __stdcall
+#endif
+
+int __stdcall this_uses_calling_conventions(int b);
